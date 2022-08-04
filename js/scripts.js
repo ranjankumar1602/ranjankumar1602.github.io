2af0594eff8584163d6a3a63339a54871f117ead
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-08-04 18:26:52 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+{/* <input type="button" onclick="location.href='https://google.com';" value="Go to Google" /> */}
